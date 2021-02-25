52bded5497795d91e609025de510eae6d3ef787a
leastbad <hello@leastbad.com>
2021-02-25 04:13:29 -0500

    optional ngrok

--- config/puma.rb
+++ config/puma.rb
@@ -42,22 +42,26 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-if ENV['RAILS_ENV'] == 'development' && `ngrok -v`.starts_with?("ngrok version")
-  require 'ngrok/tunnel'
-  options = {addr: ENV['PORT']}
-  if File.file? '.ngrok'
-    options[:config] = '.ngrok'
-  elsif File.file? ENV['HOME'] + '/.ngrok'
-    options[:config] = ENV['HOME'] + '/.ngrok'
-  end
-  if ENV['NGROK_INSPECT']
-    options[:inspect] = ENV['NGROK_INSPECT']
-  end
-  if ENV['NGROK_SUBDOMAIN']
-    options[:subdomain] = ENV['NGROK_SUBDOMAIN']
-  end
-  puts "[NGROK] tunneling at " + Ngrok::Tunnel.start(options)
-  unless ENV['NGROK_INSPECT'] == 'false'
-    puts "[NGROK] inspector web interface listening at http://localhost:4040"
+if ENV['RAILS_ENV'] == 'development'
+  begin
+    require 'ngrok/tunnel'
+    options = {addr: ENV['PORT']}
+    if File.file? '.ngrok'
+      options[:config] = '.ngrok'
+    elsif File.file? ENV['HOME'] + '/.ngrok'
+      options[:config] = ENV['HOME'] + '/.ngrok'
+    end
+    if ENV['NGROK_INSPECT']
+      options[:inspect] = ENV['NGROK_INSPECT']
+    end
+    if ENV['NGROK_SUBDOMAIN']
+      options[:subdomain] = ENV['NGROK_SUBDOMAIN']
+    end
+    puts "[NGROK] tunneling at " + Ngrok::Tunnel.start(options)
+    unless ENV['NGROK_INSPECT'] == 'false'
+      puts "[NGROK] inspector web interface listening at http://localhost:4040"
+    end
+  rescue
+    puts "[NGROK] skipped: Go to ngrok.com to install if you dare"
   end
 end
