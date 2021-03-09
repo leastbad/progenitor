4160676256f8bae798415bfc24cdd7f7753f6335
leastbad <hello@leastbad.com>
2021-03-09 05:38:01 -0500

    fixed issue with bell shake

--- app/javascript/controllers/surge/notifications_controller.js
+++ app/javascript/controllers/surge/notifications_controller.js
@@ -1,11 +1,10 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['dot']
+  static targets = ['dot', 'shake']
   static values = { unread: Boolean }
 
   connect () {
-    this.bellShake = null
     this.shakingInterval = setInterval(this.interval, 5000)
     this.element.addEventListener('show.bs.dropdown', this.dropdown)
   }
@@ -17,17 +16,16 @@ export default class extends Controller {
 
   interval = () => {
     this.dotTarget.style.display = this.unreadValue ? 'block' : 'none'
-    this.bellShake = this.bellShake || this.element.querySelector('.bell-shake')
     if (this.unreadValue)
-      this.bellShake.classList[
-        this.bellShake.classList.contains('shaking') ? 'remove' : 'add'
+      this.shakeTarget.classList[
+        this.shakeTarget.classList.contains('shaking') ? 'remove' : 'add'
       ]('shaking')
   }
 
   dropdown = () => {
     this.unreadValue = false
     clearInterval(this.shakingInterval)
-    this.bellShake.classList.remove('shaking')
+    this.shakeTarget.classList.remove('shaking')
     this.dotTarget.style.display = 'none'
   }
 }
