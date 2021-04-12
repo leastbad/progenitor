9b5ae63eee8cbb5d78b65483116a4c7712641b40
leastbad <hello@leastbad.com>
2021-04-12 00:23:05 -0400

    published chrono_trigger

--- app/events/application_event.rb
+++ app/events/application_event.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'chrono_trigger/event'
-
 class ApplicationEvent < ChronoTrigger::Event
   include CableReady::Broadcaster
   delegate :render, to: ApplicationController
