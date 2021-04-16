bbb1893565b98c874c8a4c967df31a2373672a61
leastbad <hello@leastbad.com>
2021-04-15 23:27:41 -0400

    added state, duration, time to data attributes

--- app/javascript/controllers/surge/youtube_controller.js
+++ app/javascript/controllers/surge/youtube_controller.js
@@ -12,6 +12,7 @@ export default class extends Controller {
   initialize () {
     this.width = this.widthValue || 640
     this.height = this.heightValue || 480
+    this.element['youtube'] = this
   }
 
   connect () {
@@ -21,12 +22,34 @@ export default class extends Controller {
       height: this.height,
       videoId: this.codeValue
     })
-    player.on('ready', e => (this.player = e.target))
-    player.on('stateChange', e => (this.playerState = e.data))
+    player.on('ready', e => {
+      this.element.setAttribute('data-duration', e.target.getDuration())
+      this.youtube = e.target
+      this.element.setAttribute('data-time', this.time)
+      this.element.setAttribute('data-state', -1)
+    })
+    player.on('stateChange', e => {
+      this.element.setAttribute('data-state', e.data)
+      this.element.setAttribute('data-time', this.time)
+      e.data === 1 ? this.startTimer() : clearInterval(this.timer)
+    })
   }
 
   teardown () {
     this.player.destroy()
+  }
+
+  startTimer () {
+    this.timer = setInterval(() => {
+      this.element.setAttribute('data-time', this.time)
+      this.element.dispatchEvent(
+        new CustomEvent('youtube', {
+          bubbles: false,
+          cancelable: false,
+          detail: { time: this.time }
+        })
+      )
+    }, 1000)
   }
 
   play = () => this.player.playVideo()
@@ -34,13 +57,19 @@ export default class extends Controller {
   stop = () => this.player.stopVideo()
   seek = seconds => this.player.seekTo(seconds)
 
-  get currentTime () {
-    return this.player.getCurrentTime()
+  get player () {
+    return this.youtube
+  }
+  get time () {
+    return Math.round(this.player.getCurrentTime())
   }
   get duration () {
     return this.player.getDuration()
   }
   get state () {
-    return this.playerState
+    return this.element.getAttribute('data-state')
+  }
+  get loaded () {
+    return this.player.getVideoLoadedFraction()
   }
 }
