dea22f6d8656c73e4721b9e9f9afa755a017a6a8
leastbad <hello@leastbad.com>
2021-06-07 23:56:22 -0400

    moved redis integer warning out of initializer

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Redis.exists_returns_integer = false
 
 module Progenitor
   class Application < Rails::Application
