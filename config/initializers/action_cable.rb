1aa9c5055bbf271d90987fe983673dac9d60b1b3
leastbad <hello@leastbad.com>
2021-06-15 22:56:16 -0400

    initializer cleaning

--- config/initializers/action_cable.rb
+++ config/initializers/action_cable.rb
@@ -1,17 +1,3 @@
 # frozen_string_literal: true
 
 ActionCable.server.config.logger = Logger.new(nil)
-
-# this code is/was intended to facilitate server-side AC disconnects
-# however, with the recent addition of exponential falloffs,
-# client-side disconnects are preferable... making this somewhat moot
-# module ActionCable
-#   class RemoteConnections
-#     class RemoteConnection
-#       def valid_identifiers?(ids)
-#         keys = ids.keys
-#         identifiers.any? { |id| keys.include?(id) }
-#       end
-#     end
-#   end
-# end
