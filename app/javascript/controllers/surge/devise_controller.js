6db83bb34f23446f98eb814388301863cdc71f04
leastbad <hello@leastbad.com>
2021-09-21 04:03:11 -0400

    sign in page working with twilio

--- app/javascript/controllers/surge/devise_controller.js
+++ app/javascript/controllers/surge/devise_controller.js
@@ -21,45 +21,62 @@ export default class extends ApplicationController {
     super.connect()
     this.element.addEventListener('ajax:response:error', this.loginFailed)
     this.modalTarget.addEventListener('shown.bs.modal', this.focus)
+    this.modalTarget.addEventListener('hidden.bs.modal', this.clearOtp)
   }
 
   disconnect () {
     this.element.removeEventListener('ajax:response:error', this.loginFailed)
     this.modalTarget.removeEventListener('shown.bs.modal', this.focus)
+    this.modalTarget.removeEventListener('hidden.bs.modal', this.clearOtp)
   }
 
   loginFailed = async event => {
     event.preventDefault()
     this.errorsTarget.classList.replace('d-none', 'd-flex')
     this.messageTarget.textContent = await event.detail.fetchResponse.text()
+    this.formChecked = true
     this.otpTarget.focus()
   }
 
   proceed (e) {
-    if (this.formChecked) return
-
+    if (this.formChecked) {
+      this.formChecked = undefined
+      return
+    }
     if (this.element.checkValidity()) {
-      e.preventDefault()
-      this.stimulate(
-        'Profile#otp_enabled',
-        { serializeForm: false },
-        this.emailTarget.value
-      ).then(({ payload }) => {
-        this.numberTarget.textContent = payload.number
-        this.smsTarget.classList[payload.sms ? 'remove' : 'add']('d-none')
-        payload.tfa ? this.modal.show() : this.submit()
-      })
+      if (e) e.preventDefault()
+      this.otpTarget.value.length === 6
+        ? this.submitForm(true)
+        : this.stimulate(
+            'Profile#otp_enabled',
+            { serializeForm: false },
+            this.emailTarget.value
+          ).then(({ payload }) => {
+            this.numberTarget.textContent = payload.number
+            this.smsTarget.classList[payload.sms ? 'remove' : 'add']('d-none')
+            payload.tfa ? this.modal.show() : this.submitForm(false)
+          })
     }
   }
 
+  submitForm = hide => {
+    if (this.formChecked == false) return
+    this.formChecked = true
+    if (hide) this.modal.hide()
+    this.submitTarget.click()
+    this.formChecked = false
+  }
+
   focus = () => {
+    this.errorsTarget.classList.replace('d-flex', 'd-none')
     this.otpTarget.focus()
   }
 
-  submit = () => {
-    this.formChecked = true
-    this.modal.hide()
-    this.submitTarget.click()
-    this.formChecked = false
+  count = () => {
+    if (this.otpTarget.value.length === 6) this.proceed()
+  }
+
+  clearOtp = () => {
+    this.otpTarget.value = ''
   }
 }
