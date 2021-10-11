ddde58fd3dd79326a070021ce3d494b1707da4ae
leastbad <hello@leastbad.com>
2021-10-11 00:27:20 -0400

    fixed file uploader

--- app/models/concerns/uploadable.rb
+++ app/models/concerns/uploadable.rb
@@ -7,18 +7,18 @@ module Uploadable
 
     def file_slots
       _file_slots.members.map do |slot|
-        UploadedFile.new(slot)
+        UploadedFile.new.from_json(slot)
       end
     end
 
     def add_file_slot(**args)
       uploaded_file = UploadedFile.new(uuid: "U#{SecureRandom.urlsafe_base64}", **args)
-      _file_slots << uploaded_file
+      _file_slots << uploaded_file.to_json
       uploaded_file
     end
 
     def remove_file_slot(uuid:)
-      slot = _file_slots.members.find { |temp| temp["uuid"] == uuid }
+      slot = _file_slots.members.find { |temp| JSON.parse(temp)["uuid"] == uuid }
       _file_slots.remove(slot)
     end
   end
--- app/reflexes/upload_reflex.rb
+++ app/reflexes/upload_reflex.rb
@@ -16,15 +16,29 @@ class UploadReflex < ApplicationReflex
   end
 
   def submit
-    uploaded_file = current_user.uploaded_files.create(uploaded_file_params)
-    uuid = uploaded_file_params[:uuid]
+    # ActiveStorage::Blob.create_and_upload!(io: file, filename: filename).tap do |blob|
+    #   ActiveStorage::Attachment.create!(record: record, name: name, blob: blob)
+    # end
+
+    uploaded_file = nil
+    until uploaded_file do
+      begin
+        uploaded_file = current_user.uploaded_files.create(uploaded_file_params)
+      rescue NoMethodError
+      end
+    end
+
+    uuid = uploaded_file.uuid
+
     cable_ready[UsersChannel].morph(selector: "#file-#{uuid}", html: render(
       partial: "uploaded_files/uploaded_file",
       locals: {
         uploaded_file: uploaded_file
       }
     )).broadcast_to(current_user)
+
     current_user.remove_file_slot(uuid: uuid)
+
     morph :nothing
   end
 
