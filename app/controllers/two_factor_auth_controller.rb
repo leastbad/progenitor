263afcbb228ce14a8d94946115328339213fc2f2
leastbad <hello@leastbad.com>
2021-10-14 23:31:53 -0400

    progress rebuilding 2fa settings ui

--- app/controllers/two_factor_auth_controller.rb
+++ app/controllers/two_factor_auth_controller.rb
@@ -4,9 +4,7 @@ class TwoFactorAuthController < ApplicationController
   before_action :real_users_only!
 
   def tfa
-    if current_user.otp_required_for_login?
-      @tfa_qr_code = current_user.otp_provisioning_uri(current_user.email, issuer: Rails.application.class.module_parent_name)
-    end
+    @tfa_qr_code = current_user.otp_provisioning_uri(current_user.email, issuer: Rails.application.class.module_parent_name)
 
     # codes = current_user.generate_otp_backup_codes!
     # current_user.save!
