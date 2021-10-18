d02d11a119fd303d18377de1a670bc3df8aec991
leastbad <hello@leastbad.com>
2021-10-17 23:46:59 -0400

    seeds can't first_or_create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-User.first_or_create! email: "steve@test.com", name: "Steve", password: "topsecret", password_confirmation: "topsecret", demo: true
-User.first_or_create! email: "jill@test.com", name: "Jill", password: "topsecret", password_confirmation: "topsecret", demo: true
-User.first_or_create! email: "anne@test.com", name: "Anne", password: "topsecret", password_confirmation: "topsecret", demo: true
-User.first_or_create! email: "mike@test.com", name: "Mike", password: "topsecret", password_confirmation: "topsecret", demo: true
-User.first_or_create! email: "casey@test.com", name: "Casey", password: "topsecret", password_confirmation: "topsecret", demo: true
+User.create! email: "steve@test.com", name: "Steve", password: "topsecret", password_confirmation: "topsecret", demo: true
+User.create! email: "jill@test.com", name: "Jill", password: "topsecret", password_confirmation: "topsecret", demo: true
+User.create! email: "anne@test.com", name: "Anne", password: "topsecret", password_confirmation: "topsecret", demo: true
+User.create! email: "mike@test.com", name: "Mike", password: "topsecret", password_confirmation: "topsecret", demo: true
+User.create! email: "casey@test.com", name: "Casey", password: "topsecret", password_confirmation: "topsecret", demo: true
 
 unless Customer.exists?
   125.times do
