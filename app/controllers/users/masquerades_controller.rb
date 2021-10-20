cbfcb860d0ef0005f783696419b722415f9888c6
leastbad <hello@leastbad.com>
2021-10-19 23:49:06 -0400

    remove cruft from masquerades

--- app/controllers/users/masquerades_controller.rb
+++ app/controllers/users/masquerades_controller.rb
@@ -13,15 +13,9 @@ class Users::MasqueradesController < Devise::MasqueradesController
 
   def after_masquerade_path_for(resource)
     request.referer
-    # return user_path(resource) if resource.has_role? :chef
-    # root_path
   end
 
   def after_back_masquerade_path_for(resource)
     request.referer
   end
-
-  # def find_resource
-  #   masqueraded_resource_class.friendly.find(params[:id])
-  # end
 end
